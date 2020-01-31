99d046e2e3fe5120ef1a60f9247c8bd1e308a082
zk <liuzq7@gmail.com>
2020-01-31 15:35:11 +0800

    ad language template

--- python/log/logx/setup_logging.py
+++ python/log/logx/setup_logging.py
@@ -6,6 +6,7 @@ import logging
 from  .color  import color
 
 def setup_logging(default_path='logging.yaml', default_level=logging.DEBUG, env_key='LOG_CFG'):
+    mydir = os.path.dirname(os.path.abspath(__file__))
     path = default_path
     path = os.path.join(mydir,path)
     value = os.getenv(env_key, None)

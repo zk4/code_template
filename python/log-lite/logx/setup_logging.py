a815181f68d250c2ecfbea5f717c323c85440739
zk <liuzq7@gmail.com>
2020-02-24 02:54:52 +0800

    tidy

--- python/log-lite/logx/setup_logging.py
+++ python/log-lite/logx/setup_logging.py
@@ -3,7 +3,6 @@ import os
 import yaml
 import logging.config
 import logging
-from  .color  import color
 
 def setup_logging(default_path='logging.yaml', default_level=logging.DEBUG, env_key='LOG_CFG'):
     mydir = os.path.dirname(os.path.abspath(__file__))
@@ -24,9 +23,3 @@ def setup_logging(default_path='logging.yaml', default_level=logging.DEBUG, env_
         logging.basicConfig(level=default_level)
         print('.Failed to load configuration file. Using default configs')
 
-
-    # logging.addLevelName( logging.INFO, color.G+"%s\033[1;0m" % logging.getLevelName(logging.INFO))
-    # logging.addLevelName( logging.DEBUG, color.O+"%s\033[1;0m" % logging.getLevelName(logging.DEBUG))
-    # logging.addLevelName( logging.WARNING, "\033[1;31m%s\033[1;0m" % logging.getLevelName(logging.WARNING))
-    # logging.addLevelName( logging.ERROR, "\033[1;41m%s\033[1;0m" % logging.getLevelName(logging.ERROR))
-    # logging.addLevelName( logging.CRITICAL, "\033[1;41m%s\033[1;0m" % logging.getLevelName(logging.CRITICAL))

edac64847d3f095723b561d4c742b61c52844735
zk <liuzq7@gmail.com>
2020-02-24 13:23:43 +0800

    move config outside

--- python/log-lite/logx/setup_logging.py
+++ python/log-lite/logx/setup_logging.py
@@ -8,10 +8,10 @@ logger = logging.getLogger(__name__)
 def setup_logging(default_path='logging.yaml', default_level=logging.DEBUG, env_key='LOG_CFG'):
     mydir = os.path.dirname(os.path.abspath(__file__))
     path = default_path
-    path = os.path.join(mydir,path)
-    value = os.getenv(env_key, None)
-    if value:
-        path = value
+    # path = os.path.join(mydir,path)
+    # value = os.getenv(env_key, None)
+    # if value:
+    #     path = value
     if os.path.exists(path):
         with open(path, 'rt') as f:
             try:

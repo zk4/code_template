c0c4d5c3cb46abdf0d4276dd1463ddd07d555b23
zk <liuzq7@gmail.com>
2020-02-24 17:04:22 +0800

    add makefile main entry

--- python/best_practice/main.py
+++ python/best_practice/main.py
@@ -1,18 +1,12 @@
 #coding: utf-8
 from rabbit.logx import  *
+from rabbit import entry_point
 import logging
 
 logger = logging.getLogger(__name__)
 
 
 if __name__ == "__main__":
-    logger.debug("debug")
-    logger.info("hello cinfo")
-    logger.info("hello cinfo")
-    logger.info("hello cinfo")
-    logger.info("hello cinfo")
-    logger.error('error ')
-    logger.warning('warning')
-    logger.critical('critical')
+    entry_point()
 
 

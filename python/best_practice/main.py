fe0e7f45eb1ceaab69ec87023550e4e659913b48
zk <liuzq7@gmail.com>
2020-02-24 17:17:24 +0800

    update

--- python/best_practice/main.py
+++ python/best_practice/main.py
@@ -1,11 +1,5 @@
 #coding: utf-8
-from rabbit.logx import  *
 from rabbit import entry_point
-import logging
-
-logger = logging.getLogger(__name__)
-
-
 if __name__ == "__main__":
     entry_point()
 

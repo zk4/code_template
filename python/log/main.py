59a790f4c3c0b6d290039f61f5b3d084409b7c29
zk <liuzq7@gmail.com>
2020-02-24 15:40:16 +0800

    before

--- python/log/main.py
+++ python/log/main.py
@@ -1,14 +1,15 @@
 #coding: utf-8
 import logx
 import logging
+import mylib
 logger = logging.getLogger(__name__)
+logger.setLevel("CRITICAL")
 
 if __name__ == "__main__":
-    logger = logger or logging.getLogger(__name__)
-
     logger.debug("debug")
     logger.info("hello cinfo")
     logger.error('error ')
     logger.warning('warning')
     logger.critical('critical')
+
 
---
+++ python/log/mylib/__init__.py
@@ -0,0 +1,8 @@
+import logging
+logger = logging.getLogger(__name__)
+logger.debug("debug")
+logger.info("hello cinfo")
+logger.error('error ')
+logger.warning('warning')
+logger.critical('critical')
+

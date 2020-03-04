fec6ee9c45bd3bfba191317478719541917958d8
zk <liuzq7@gmail.com>
2020-03-04 15:37:49 +0800

    one jar

--- java/best_practice/rabbit/src/main/java/com/zk/App.java
+++ java/best_practice/rabbit/src/main/java/com/zk/App.java
@@ -1,13 +1,18 @@
 package com.zk;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+	  private static final Logger logger = LoggerFactory.getLogger(App.class);
     public static void main( String[] args )
     {
+			  logger.debug("Hello from Logback");
         System.out.println( "Hello rabbit!" );
     }
 }

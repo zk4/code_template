200f6e2c5f5d92152287ae10fe6111bca58676c2
zk <liuzq7@gmail.com>
2020-03-16 23:16:08 +0800

    update

--- c/opengl/bestpractice/bestpractice.c
+++ c/opengl/bestpractice/bestpractice.c
@@ -79,6 +79,8 @@ void initShader()
 
     program = LoadProgram( vert, NULL, frag );
 
+}
+void prepareData(){
     glGenVertexArrays( 1, &VAO );
     glBindVertexArray( VAO );
 
@@ -119,10 +121,10 @@ void renderScene(GLFWwindow* window){
 
 				Matrix4f World;
 
-				World.m[0][0] = 1.0f; World.m[0][1] = 0.0f; World.m[0][2] = 0.0f; World.m[0][3] = sinf(Scale);
-				World.m[1][0] = 0.0f; World.m[1][1] = 1.0f; World.m[1][2] = 0.0f; World.m[1][3] = 0.0f;
-				World.m[2][0] = 0.0f; World.m[2][1] = 0.0f; World.m[2][2] = 1.0f; World.m[2][3] = 0.0f;
-				World.m[3][0] = 0.0f; World.m[3][1] = 0.0f; World.m[3][2] = 0.0f; World.m[3][3] = 1.0f;
+			World.m[0][0]=sinf(Scale); World.m[0][1]=0.0f;        World.m[0][2]=0.0f;        World.m[0][3]=0.0f;
+			World.m[1][0]=0.0f;        World.m[1][1]=sinf(Scale); World.m[1][2]=0.0f;        World.m[1][3]=0.0f;
+			World.m[2][0]=0.0f;        World.m[2][1]=0.0f;        World.m[2][2]=sinf(Scale); World.m[2][3]=0.0f;
+			World.m[3][0]=0.0f;        World.m[3][1]=0.0f;        World.m[3][2]=0.0f;        World.m[3][3]=1.0f;
 
     		GLuint gWorldLocation = glGetUniformLocation(program, "gWorld");
 				glUniformMatrix4fv(gWorldLocation, 1, GL_TRUE, &World.m[0][0]);
@@ -197,6 +199,7 @@ int main( int argc, char** argv )
 
 
     initShader();
+		prepareData();
     glfwSetCursorPosCallback(window, cursorPosFun);
     glfwSetMouseButtonCallback(window, mouseButtonFun);
 		renderScene(window);

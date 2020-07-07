7490f048268541640c1c22fac6552e3869f2e46b
zk <liuzq7@gmail.com>
2020-07-07 23:05:38 +0800

    divide prod and dev webpack

--- js/webpack/bestpractice/webpack.config.js
+++ js/webpack/bestpractice/webpack.config.js
@@ -1,45 +1,43 @@
-
-const path = require('path');
+const path = require("path");
 // 用于引用webpack内置插件
 const webpack = require("webpack");
 // 外部插件
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-	entry: {
-		main: "./src/index.js",
-	},
-	output: {
-		// [name] will follow the dir name
-		filename: "[name].js",
-		path: __dirname + "/dist",
-	},
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				exclude: /node_module/,
-				use: "babel-loader",
-			},
-			{
+  entry: {
+    main: "./src/index.js",
+  },
+  output: {
+    // [name] will follow the dir name
+    filename: "[name].js",
+    path: __dirname + "/dist",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_module/,
+        use: "babel-loader",
+      },
+      {
         test: /\.(png|svg|jpg|gif)$/,
-        include: [
-          path.resolve(__dirname, "src/components")
-        ],
-				use:[
-					'file-loader'
-				]
-			},
-			{
-				test: /\.css$/,
-				use: ["style-loader", "css-loader"], // 注意排列顺序，执行顺序与排列顺序相反
-			},
-		],
-	},
-	plugins: [
-    // auto reload 
-		new webpack.HotModuleReplacementPlugin(),
+        include: [path.resolve(__dirname, "src/components")],
+        use: ["file-loader"],
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"], // 注意排列顺序，执行顺序与排列顺序相反
+      },
+    ],
+  },
+  plugins: [
+    // auto reload
+    new webpack.HotModuleReplacementPlugin(),
     // auto inject
-		new HtmlWebpackPlugin({ template: "./src/index.html" }),
-	],
+    new HtmlWebpackPlugin({
+      title: "this is the title",
+      template: "./src/index.html",
+    }),
+  ],
 };

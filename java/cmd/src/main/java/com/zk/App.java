2e6b49e6b4d8884cdd4ab46ebb55610660fe40ad
zk <liuzq7@gmail.com>
2022-07-01 22:34:07 +0800

    chore: u

--- java/cmd/src/main/java/com/zk/App.java
+++ java/cmd/src/main/java/com/zk/App.java
@@ -1,18 +1,13 @@
 package com.zk;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 /**
  * Hello world!
  *
  */
 public class App 
 {
-	  private static final Logger logger = LoggerFactory.getLogger(App.class);
     public static void main( String[] args )
     {
-			  logger.info("Hello from Logback here baby!");
         System.out.println( "Hello! cmd project..," );
     }
 }

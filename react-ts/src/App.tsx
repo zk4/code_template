684e38e7374bcb4a9ed52afd3d5a2c9fa104b940
zk <liuzq7@gmail.com>
2022-09-03 16:52:52 +0800

    chore: u

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from './modules/button'
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Button/>
     </div>
   );
 }
---
+++ react-ts/src/modules/button/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import styles from './index.module.css'
+
+const Button: React.FC = () => {
+ return <button className={styles.blue}>hello,world</button>
+}
+export default Button;

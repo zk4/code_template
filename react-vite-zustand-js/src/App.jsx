f9aee15aeeb0545588ae5a680e90ab02c91d97d1
zk <liuzq7@gmail.com>
2023-04-28 20:15:11 +0800

    chore: u

--- react-vite-zustand-js/src/App.jsx
+++ react-vite-zustand-js/src/App.jsx
@@ -3,24 +3,22 @@ import useArrayStore from "./store";
 import Btn from "./components/btn";
 
 function App() {
-	const { arr, updateArr, updateValue } = useArrayStore();
+  const { arr, updateArr, updateValue } = useArrayStore();
 
-	return (
-		<div>
+  return (
+    <div>
       hello
-			<button onClick={() => updateArr([])}>
-				Reset Array to []
-			</button>
-			<button onClick={() => updateValue(1, 4)}>Update 2th Value</button>
-			<Btn index={3} v={"hello"}>
-				update 3th vaule to hello
-			</Btn>
-			<ul>
-				{arr.map((item, index) => {
-					return <li key={index}>{item}</li>;
-				})}
-			</ul>
-		</div>
-	);
+      <button onClick={() => updateArr([])}>Reset Array to []</button>
+      <button onClick={() => updateValue(1, 4)}>Update 2th Value</button>
+      <Btn index={3} v={"hello"}>
+        update 3th vaule to hello
+      </Btn>
+      <ul>
+        {arr.map((item, index) => {
+          return <li key={index}>{item}</li>;
+        })}
+      </ul>
+    </div>
+  );
 }
 export default App;

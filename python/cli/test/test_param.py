3d9051011d0da42e299228fb75f1826a21663fe1
zk <liuzq7@gmail.com>
2023-08-29 19:01:05 +0800

    u

---
+++ python/cli/test/test_param.py
@@ -0,0 +1,17 @@
+import pytest
+
+def add(a, b):
+    return a + b
+
+@pytest.mark.parametrize("a, b, expected", [
+    (1, 2, 3),
+    (0, 0, 0),
+])
+@pytest.mark.parametrize("description", [
+    "positive numbers",
+    "zeroes",
+])
+def test_addition(a, b, expected, description):
+    print(a,b,expected,description)
+    result = add(a, b)
+    assert result == expected, description

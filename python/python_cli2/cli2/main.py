389ebf714f6fa3ca48db2c7fd6f9e4fc4926a666
zk <liuzq7@gmail.com>
2023-09-20 12:29:38 +0800

    u

--- python/python_cli2/cli2/main.py
+++ python/python_cli2/cli2/main.py
@@ -6,8 +6,11 @@ from .module1.d import func3
 from .module1 import func2_c
 import cli2.module1.e as e
 from .module1.e import func4
+from .module2.f import func5,func6
 
 def hello(name: str = "world"):
+    func6()
+    func5()
     func4()
     e.func4()
     func2_c()
---
+++ python/python_cli2/cli2/module2/f.py
@@ -0,0 +1,12 @@
+
+
+from ..module1.e import func4
+from cli2.module1.e import func4 as func4_c
+
+def func5():
+    func4()
+
+def func6():
+    func4_c()
+
+

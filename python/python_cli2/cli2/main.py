7b6015a973daf350d7610254bd4800fc8f823b69
zk <liuzq7@gmail.com>
2023-09-20 12:58:35 +0800

    u

--- python/python_cli2/cli2/main.py
+++ python/python_cli2/cli2/main.py
@@ -3,12 +3,15 @@
 from .a import func1
 from .module1.c import func2
 from .module1.d import func3
+# this is module.__init__.py file
 from .module1 import func2_c
 import cli2.module1.e as e
+import cli2
 from .module1.e import func4
 from .module2.f import func5,func6
 
 def hello(name: str = "world"):
+    cli2.module1.e.func4()
     func6()
     func5()
     func4()
